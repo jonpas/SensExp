4f6cde970862b33e715a573f581459b7a3ce6f99
jonpas <jonpas33@gmail.com>
2020-04-30 01:05:11 +0200

    Fix typo

--- test/mqtt_mosquitto_default.sh
+++ test/mqtt_mosquitto_default.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Duplicate this file to change information
-mosquitto_sub -v -h <server-uri -p <port> -t '<topic>' -u <username> -P <password>
+mosquitto_sub -v -h <server-uri> -p <port> -t '<topic>' -u <username> -P <password>
